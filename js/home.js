0347faeed9cfae2a56a6b85ffca6ff6fe98c7c06
satish <satishmalkar94@gmail.com>
2019-07-06 14:18:51 +0530

    Enhancement

--- js/home.js
+++ js/home.js
@@ -1,33 +1,25 @@
 var app = angular.module('angularApp', []);
+
+
 app.controller('formCtrl', function($scope,angularService) {
-    // ineject the service
-    counterNum = 0;
+
+$scope.ShowBtn = function () {
+    $scope.inVisible = true;
+  };
+   counterNum = 0;
     $scope.incCounter = function(){
         angularService.setProperty(angularService.getProperty()+1);
         $scope.counterNum = angularService.getProperty();
-        // console.log("incCounter :: "+$scope.counterNum);
-
     };
     $scope.dcrCounter = function(){
         angularService.setProperty(angularService.getProperty()-1);
         $scope.counterNum = angularService.getProperty();
-        // console.log("dcrCounter :: "+$scope.counterNum);
-
     };
-    $scope.textValue1 = function(){
-        $scope.textValue1="test"
-        $scope.value="satish"
-
-
-    }
-
 
 });
 
-
 app.service('angularService',function(){
     var counterN = 0;
-
     return {
             getProperty: function () {
                 return counterN;
@@ -38,21 +30,59 @@ app.service('angularService',function(){
         };
 })
 
-// app.service('label',function(){
-//     var label
-//     return label;
-
-// })
-
 
 app.directive('myFirstScript',function(){
 
-return{
-template :"<div ndclass='col-md-12'><div class='addremove' id='addremove'><i class='fa fa-minus-circle' id='plusi' ng-click='dcrCounter()'></i><span class='counter'id='total'><label id='lblText'></label><label id='lblCounter'>{{counterNum | number:0}}</label></span><i class='fa fa-plus-circle' id='plusj' ng-click='incCounter()'></i></div></div>"
-    };
 
+return{
+
+
+template :"<div ndclass='col-md-12'><div class='addremove' id='addremove'><i class='fa fa-minus-circle' id='plusi' ng-click='dcrCounter()'></i><span class='counter'id='total'><label id='lblText'></label><label id='lblCounter'>{{counterNum | number:0}}</label></span><i class='fa fa-plus-circle' id='plusj' ng-click='incCounter()'></i></div></div>",
+
+    restrict: 'E',
+    link:function(scope,elm,attr) {
+       scope.count = 0;
+       scope.incCounter = function(){
+       scope.counterNum = scope.counterNum + 1
+       return scope.counterNum;
+       scope.dcrCounter = function(){
+        scope.counterNum = scope.counterNum -1
+       return scope.counterNum;
+
+       }
+       }
+
+    }
+}
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // app.controller('formCtrl',function(){
@@ -68,3 +98,9 @@ template :"<div ndclass='col-md-12'><div class='addremove' id='addremove'><i cla
 //     }
 // })
 
+// var demoApp = angular.module(‘demoApp’,[]);
+
+// demoApp.controller( DemoController, function ( $scope ) {
+//     console.log($scope)
+
+// });
