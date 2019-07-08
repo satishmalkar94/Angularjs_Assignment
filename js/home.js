9dc3826e3f5d3f17b6e045bbf2624650aaeb3ce7
satish <satishmalkar94@gmail.com>
2019-07-08 10:31:32 +0530

    Features ::changes in html and js code

--- js/home.js
+++ js/home.js
@@ -1,55 +1,57 @@
 var app = angular.module('angularApp', []);
 
+app.service('angularService',function(){
+
+    this.addControl = function($scope)
+    {
+        var labelData = {
+            lblValue : 0,
+            lblStr : $scope.textvalue,
+            lblColor : $scope.cvalue
+        };
+        $scope.controls.push(labelData);
+    }
+});
 
 app.controller('formCtrl', function($scope,angularService) {
 
 $scope.ShowBtn = function () {
-    $scope.inVisible = true;
+    angularService.addControl($scope);
   };
-   counterNum = 0;
-    $scope.incCounter = function(){
-        angularService.setProperty(angularService.getProperty()+1);
-        $scope.counterNum = angularService.getProperty();
+    $scope.incCounter = function(index){
+        $scope.controls[index].lblValue ++;
     };
-    $scope.dcrCounter = function(){
-        angularService.setProperty(angularService.getProperty()-1);
-        $scope.counterNum = angularService.getProperty();
+    $scope.dcrCounter = function(index){
+        $scope.controls[index].lblValue --;
     };
+
+    $scope.controls = [];
+    $scope.cvalue = "#000000"
 
 });
 
-app.service('angularService',function(){
-    var counterN = 0;
-    return {
-            getProperty: function () {
-                return counterN;
-            },
-            setProperty: function(value) {
-                counterN = value;
-            }
-        };
-})
+
 
 app.directive('myFirstScript',function(){
 
 
 return{
-template :"<div ndclass='col-md-12'><div class='addremove' id='addremove'><i class='fa fa-minus-circle' id='plusi' ng-click='dcrCounter()'></i><span class='counter'id='total'><label id='lblText'></label><label id='lblCounter'>{{counterNum | number:0}}</label></span><i class='fa fa-plus-circle' id='plusj' ng-click='incCounter()'></i></div></div>",
+template :"<div ndclass='col-md-12' ng-repeat='item in controls track by $index'><div class='addremove' ng-style={'background-color':item.lblColor}><i class='fa fa-minus-circle' id='plusi' ng-click='dcrCounter($index)'></i><span class='counter'><label>{{item.lblStr}}</label><label id='lblCounter'>{{item.lblValue}}</label></span><i class='fa fa-plus-circle' id='plusj' ng-click='incCounter($index)'></i></div></div>",
 
-    restrict: 'E',
-    link:function(scope,elm,attr) {
-       scope.count = 0;
-       scope.incCounter = function(){
-       scope.counterNum = scope.counterNum + 1
-       return scope.counterNum;
-       scope.dcrCounter = function(){
-        scope.counterNum = scope.counterNum -1
-       return scope.counterNum;
+    // restrict: 'E',
+    // link:function(scope,elm,attr) {
+    //    scope.count = 0;
+    //    scope.incCounter = function(){
+    //    scope.counterNum = scope.counterNum + 1
+    //    return scope.counterNum;
+    //    scope.dcrCounter = function(){
+    //     scope.counterNum = scope.counterNum -1
+    //    return scope.counterNum;
 
-       }
-       }
+    //    }
+    //    }
 
-    }
+    // }
 }
 
 });
